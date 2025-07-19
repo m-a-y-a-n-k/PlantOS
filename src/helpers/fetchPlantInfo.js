0dde758f5cd3b0439cffa760f4cb3fd8ac3082a0
Mayank <mnarulasubs@gmail.com>
2025-07-19 15:04:47 +0530

    Switch to API call

--- src/helpers/fetchPlantInfo.js
+++ src/helpers/fetchPlantInfo.js
@@ -1,5 +1,32 @@
-import plants from "../data/plants.json";
+async function getToken() {
+  const params = {
+    origin: window.location.origin,
+    ip: window.location.ip,
+    token: process.env.REACT_APP_TOKEN,
+  };
+  const response = await fetch("https://trefle.io/api/auth/claim", {
+    method: "post",
+    body: JSON.stringify(params),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const { token } = await response.json();
+  return token;
+}
 
-export function fetchPlantInfo(label) {
-  return plants.find((p) => p.label.toLowerCase() === label.toLowerCase());
+export async function fetchPlantInfo(plantLabel) {
+  const token = await getToken();
+  const response = await fetch(
+    `https://trefle.io/api/v1/plants?token=${token}`,
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const plants = await response.json();
+  return plants.find(({ label }) => label === plantLabel);
 }
